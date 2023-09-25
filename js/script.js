090b192290590fb503060ca9452bcbc5f417ac52
Gabriel Moura <gabriel.moura@sighra.com.br>
2023-09-25 20:07:32 -0300

    Super Marcio Costa- ok

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+const clouds = document.querySelector('.clouds')
+const scoreDisplay = document.getElementById('score')
+let score = 0; 
+
+const modal = document.getElementById("myModal")
+const restartButton = document.getElementById("restartButton")
+
+
+function showGameOverModal() {
+    modal.style.display = "block";
+  document.getElementById("gameOverScore").textContent = `Pontuação: ${score}`
+
+}
+
+
+restartButton.addEventListener("click", function () {
+  
+  location.reload();
+});
 
 
 const jump = () =>{
@@ -13,10 +32,28 @@ const jump = () =>{
 const loop = setInterval(() =>{
     const pipe_position = pipe.offsetLeft
     const mario_position = +window.getComputedStyle(mario).bottom.replace('px' , '')
+    const cloud_position = clouds.offsetLeft
 
-    if (pipe_position <= 120 && mario_position <80) {
+    if (pipe_position <= 120 && pipe_position > 0 && mario_position <80) {
         pipe.style.animation ='none'
-        pipe.style.left = `${pipe_position}px`;
+        pipe.style.left = `${pipe_position}px`
+        clouds.style.animation ='none'
+        clouds.style.left = `${cloud_position}px`
+        mario.style.animation ='none'
+        mario.style.left = `${mario_position}px`
+        
+        mario.src = './images/game-over.png'
+        mario.style.width = '85px'
+        mario.style.marginLeft = '50px'
+
+       
+
+        clearInterval(loop)
+        showGameOverModal();
+    }
+    else if (pipe_position < 120 && pipe_position > 0) {
+        score += 1;
+        scoreDisplay.textContent = score;
     }
 
     
